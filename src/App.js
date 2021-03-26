90b728200161371adf3aa3c9172b86533521c4b1
Kittidet Cheavachirakul <fvgbhv14@gmail.com>
2021-03-26 18:58:12 +0700

    build: Github pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import HomePage from './pages/HomePage'
 import AssignmentsPage from './pages/AssignmentsPage'
 import DynamicPage from './pages/DynamicPage'
 import CounterPage from './pages/CounterPage'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 function App() {
     return (
